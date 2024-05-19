9ff3610ecec39a58644c3d93c4a6fd32e321171a
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-05-19 13:55:02 +0500

    new-assignment

--- Chapter-6-9/index.js
+++ Chapter-6-9/index.js
@@ -48,3 +48,42 @@ document.write( ' Result of --a - --b + ++b + b--  is = ' + result);
 var username = prompt('Assalam - o - Alaikum \n'  + 'Enter your Name')
 
 
+// Question 04
+// user number prompt
+document.write('<h1>Question 04</h1>');
+
+
+var userNum = prompt('Enter your lucky number:');
+
+document.write(userNum + ' x  1 = ' + userNum*1  + '<br></br>');
+document.write(userNum + ' x  2 = ' + userNum*2  + '<br></br>');
+document.write(userNum + ' x  3 = ' + userNum*3  + '<br></br>');
+document.write(userNum + ' x  4 = ' + userNum*4  + '<br></br>');
+document.write(userNum + ' x  5 = ' + userNum*5  + '<br></br>');
+document.write(userNum + ' x  6 = ' + userNum*6  + '<br></br>');
+document.write(userNum + ' x  7 = ' + userNum*7  + '<br></br>');
+document.write(userNum + ' x  8 = ' + userNum*8  + '<br></br>');
+document.write(userNum + ' x  9 = ' + userNum*9  + '<br></br>');
+document.write(userNum + ' x  10 = ' + userNum*10 + '<br></br>' );
+
+
+// Question 05
+// user number prompt
+document.write('<h1>Question 05</h1>');
+
+var sub1 = prompt('Enter subject?');
+var sub2 = prompt('Enter subject?');
+var sub3 = prompt('Enter subject?');
+
+var total_marks = 100;
+
+var sub1_Marks = +prompt('How many numbers did you get in ' + sub1);
+var sub2_Marks = +prompt('How many numbers did you get in ' + sub2);
+var sub3_Marks = +prompt('How many numbers did you get in ' + sub3);
+
+document.write('<b>Subject</b>' + '&nbsp;&nbsp;&nbsp;' +  '<b>Total Marks</b>' + '&nbsp;&nbsp;&nbsp;' +  '<b>Obt Marks</b>' +  '&nbsp;&nbsp;&nbsp;' + '<b>Percentage</b>' +  '<br></br>' );
+document.write( sub1  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  +   '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +   total_marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  sub1_Marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + (sub1_Marks/total_marks)*100 + '%' + '<br></br>');
+document.write( sub2  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  +  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +   total_marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  sub2_Marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  (sub2_Marks/total_marks)*100 + '%' + '<br></br>');
+document.write( sub3  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  +  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  total_marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  sub3_Marks + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  (sub3_Marks/total_marks)*100 + '%' + '<br></br>');
+document.write( '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  + (total_marks + total_marks + total_marks) + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + (sub1_Marks + sub2_Marks + sub3_Marks));
+
