ab7f10a965988b2431f6b917b9e7572748c8844a
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-05-26 08:52:43 +0500

    new assignment

---
+++ Chapter-14-16/index.js
@@ -0,0 +1,16 @@
+
+//Queston 1
+// empty array
+
+var studentName = [];
+
+
+// Question 2
+// empty array
+
+var studentName = [];
+
+// Question 03
+// initializa array
+
+var studentName = ['Maryam' , ' Azam' ,'Misbah'];
