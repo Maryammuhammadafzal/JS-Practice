364ee6c530e75a4c6328fb448511da554997d775
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-05-30 00:17:02 +0500

    new-assignment

--- Chapter-14-16/index.js
+++ Chapter-14-16/index.js
@@ -1,268 +1,211 @@
 
-//Queston 1
-// empty array
+// //Queston 1
+// // litteral notation array
 
-var studentName = [];
-
-
-// Question 2
-// empty array
-
-var studentName = [];
-
-// Question 03
-// string array 
-
-var siblingsName = ['Maryam' , ' Azam' ,'Misbah'];
+// var studentName = [];
 
 
-//Question 04
-// number array 
+// // Question 2
+// // object notation array
 
-var num = [81 , 79 , 56];
+// var studentName = new Array();
 
+// // Question 03
+// // string array 
 
-//Question 05
-// boolean array 
-
-var boolean = [false , true];
-
-
-//Question 06
-// mixed array 
-
-var mixed = [81 , ' Maryam' , true , undefined ];
+// var siblingsName = ['Maryam' , ' Azam' ,'Misbah'];
 
 
-//Question 07
-// education array 
+// //Question 04
+// // number array 
 
-document.write('<h1>Qualification</h1>');
-
-var qualification = ['1) SSC' , '2) HSC' , '3) BCS' , '4) BS' , '5) BCOM' , '6) MS' , '7) MPhil' , '8) PhD'];
-document.write('<h1> Qualification </h1>');
-document.write('<br>' , qualification[0] , '<br>' , qualification[1] , '<br>' , qualification[2] , '<br>' , qualification[3] , '<br>' , qualification[4] ,
-'<br>' , qualification[5] ,'<br>' , qualification[6] ,'<br>' , qualification[7] , '<br>'
-)
-
-//Question 08
-// Student score array 
-
-document.write('<h1>Students Scores</h1>');
-
-var studentsName = ['Maryam' , ' Misbah ' , 'Ayesha'];
-var studentsScore = [480 , 428 , 320];
-var totalScore = 500;
-var percentage = [(studentsScore[0] / totalScore ) *100 , (studentsScore[1] / totalScore ) *100 , (studentsScore[2] / totalScore ) *100];
-
-document.write('Score of ' + studentsName[0] + ' is ' + studentsScore[0] + ' .Percentage is '   + percentage[0] + '%'   + '  <br>'   +
-               'Score of ' + studentsName[1] + ' is ' + studentsScore[1] + ' .Percentage is ' +  percentage[1] + '%' +  '<br>'  +
-               'Score of ' + studentsName[2] + ' is ' + studentsScore[2] + ' .Percentage is ' + percentage[2] + '%' + '<br>'
-)
-
-// Question 9
-// colour array
-
-document.write('<h3>Colors</h3>');
-
-// Initialize an array with color names
-var colors = ["Red", "Green", "Blue"];
-
-// Function to display array elements in the browser
-function displayArray() {
-    document.write("<b>Current Array:</b> " + colors.join(", ") + "<br>");
-}
-
-// Function to add color to the beginning of the array
-function addColorToBeginning(color) {
-    colors.unshift(color);
-    displayArray();
-}
-
-// Function to add color to the end of the array
-function addColorToEnd(color) {
-    colors.push(color);
-    displayArray();
-}
-
-// Function to add two colors to the beginning of the array
-function addTwoColorsToBeginning() {
-    colors.unshift("Yellow", "Purple");
-    displayArray();
-}
-
-// Function to delete the first color in the array
-function deleteFirstColor() {
-    colors.shift();
-    displayArray();
-}
-
-// Function to delete the last color in the array
-function deleteLastColor() {
-    colors.pop();
-    displayArray();
-}
-
-// Function to add a color at a specific index
-function addColorAtIndex(index, color) {
-    colors.splice(index, 0, color);
-    displayArray();
-}
-
-// Function to delete colors from a specific index
-function deleteColorsFromIndex(index, numToDelete) {
-    colors.splice(index, numToDelete);
-    displayArray();
-}
-
-// Ask the user for input and perform operations accordingly
-var userInput;
-userInput = prompt("Enter the color you want to add to the beginning:");
-addColorToBeginning(userInput);
-
-userInput = prompt("Enter the color you want to add to the end:");
-addColorToEnd(userInput);
-
-addTwoColorsToBeginning();
-
-deleteFirstColor();
-
-deleteLastColor();
-
-var index = prompt("Enter the index at which you want to add a color:");
-userInput = prompt("Enter the color you want to add at index " + index + ":");
-addColorAtIndex(index, userInput);
-
-var deleteIndex = prompt("Enter the index from which you want to delete color(s):");
-var deleteCount = prompt("Enter the number of colors you want to delete from index " + deleteIndex + ":");
-deleteColorsFromIndex(deleteIndex, deleteCount);
+// var num = [81 , 79 , 56];
 
 
-//Question 10
-// ascending an array
+// //Question 05
+// // boolean array 
 
-document.write('<b>Order the score</b>');
-
-var studentScores = [87, 65, 92, 78, 90];
+// var boolean = [false , true];
 
 
-studentScores.sort(function(a, b) {
-    return a - b;
-});
+// //Question 06
+// // mixed array 
 
-document.write("Sorted student scores in ascending order: ");
-document.write(studentScores);
+// var mixed = [81 , ' Maryam' , true , undefined , 789 , false , 'azam' , null ];
+
+
+// //Question 07
+// // education array 
+
+// document.write('<h1>Qualification</h1>');
+
+// var qualification = [' SSC' , ' HSC' , ' BCS' , ' BS' , ' BCOM' , ' MS' , ' MPhil' , ' PhD'];
+
+// document.write('<h1> Qualification </h1>');
+// document.write( '<ol>' );
+// document.write('<li>' + qualification[0] + '</li>' +  '<li>' + qualification[1] + '</li>' +  '<li>' + qualification[2] + '</li>' +  '<li>' + qualification[3] + '</li>' +  '<li>' + qualification[4] + '</li>' + 
+// '<li>' + qualification[5] + '</li>' + '<li>' + qualification[6] + '</li>' + '<li>' + qualification[7] + '</li>' )
+// document.write( '</ol>' );
+
+// //Question 08
+// //+ '</li>' + Student score array 
+
+// document.write('<h1>Students Scores</h1>');
+
+// var studentsName = ['Maryam' , ' Misbah ' , 'Ayesha'];
+// var studentsScore = [480 , 428 , 320];
+// var totalScore = 500;
+// var percentage = [(studentsScore[0] / totalScore ) *100 , (studentsScore[1] / totalScore ) *100 , (studentsScore[2] / totalScore ) *100];
+
+// document.write('Score of ' + studentsName[0] + ' is ' + studentsScore[0] + ' .Percentage is '   + percentage[0] + '%'   + '  <br>'   +
+//                'Score of ' + studentsName[1] + ' is ' + studentsScore[1] + ' .Percentage is ' +  percentage[1] + '%' +  '<br>'  +
+//                'Score of ' + studentsName[2] + ' is ' + studentsScore[2] + ' .Percentage is ' + percentage[2] + '%' + '<br>'
+// )
+
+// // Question 9
+// // colour array
+
+// document.write('<h3>Colors</h3>');
+
+// // Initialize an array with color names
+// var colors = ["Red", "Green", "Blue"];
+
+// // Display array elements in the browser
+//  document.write(colors + "<br>");
+
+
+// // Ask the user to add color to the beginning of the array
+// var addColorInStart = prompt('What color you want to add in the begining?');
+
+// // Display added color to the beginning of the array
+//  colors.unshift(addColorInStart);
+// document.write(colors + "<br>");
+
+
+// // Ask the user to add color to the end of the array
+// var addColorInEnd = prompt('What color you want to add in the end?');
+
+// // Display added color to the end of the array
+// colors.push(addColorInEnd);
+// document.write(colors + "<br>");
+
+// // Display that add two colors to the beginning of the array
+//     colors.unshift("Yellow", "Purple");
+//     document.write(colors + "<br>");
+
+
+// // Delete the first color in the array
+//  colors.shift();
+//  document.write(colors + "<br>");
+
+// // Delete the last color in the array
+// colors.pop();
+// document.write(colors + "<br>");
+
+// // Add a color at a specific index
+// var addColorAtIndex = +prompt('On which index number you want add new color?');
+// var colorNameAtIndex = prompt('please describe the color name which you want to add at number ' + addColorAtIndex + ' :');
+// colors.splice(addColorAtIndex, 0, colorNameAtIndex);
+// document.write(colors + "<br>");
+
+
+// // Delete a color at a specific index
+// var delColorAtIndex = +prompt('On which index number you want delete the color?');
+// var delcolorNumber = +prompt('How many colors you want to delete from the index number ' + delColorAtIndex + ' :');
+// colors.splice(delColorAtIndex , delcolorNumber);
+// document.write(colors + "<br>");
 
 
 
-//Question 11
-// select cities array
+// //Question 10
+// // ascending an array
 
-document.write('<h1>Cities</h1>');
+// document.write('<b>Order the score</b>' + "<br>");
 
-var cities = ["Karachi", "Lahore", "Islamabad", "peshawar", "Quetta"];
+// var studentScores = [87, 65, 92, 78, 90];
 
-// Initialize an empty array for selected cities
-let selectedCities = [];
+// studentScores.sort()
 
-// Copy three array elements from cities array to selectedCities array
-selectedCities = cities.slice(0, 3);
+// document.write("Sorted student scores in ascending order: ");
+// document.write(studentScores + "<br>");
 
-document.write('<br>'+ "Selected cities: ");
-document.write(selectedCities);
 
-//Question 12
-// joining an array
+// //Question 11
+// // select cities array
 
-document.write('<h1>Joined array</h1>');
+// document.write('<h1>Cities</h1>');
 
-var arr = ["This", "is", "my", "cat"];
+// var cities = ["Karachi", "Lahore", "Islamabad", "peshawar", "Quetta"];
 
-var joined = arr.join(' ');
+// // Initialize an empty array for selected cities
+// let selectedCities = [];
 
-document.write('<br>' + arr + '<br>');
-document.write(joined);
+// // Copy three array elements from cities array to selectedCities array
+// selectedCities = cities.slice(0, 3);
+
+// document.write('<br>'+ "Selected cities: ");
+// document.write(selectedCities);
+
+// //Question 12
+// // joining an array
+
+// document.write('<h1>Joined array</h1>');
+
+// var arr = ["This", "is", "my", "cat"];
+
+// var joined = arr.join(' ');
+
+// document.write('<br>' + arr + '<br>');
+// document.write(joined);
 
 //Question 13
 // one by one calling array 
 document.write('<h1> Devices </h1>');
 
-var device = [];
-
-device = ['Monitor ',];
+var device = ['Monitor ','Keyboard ','CPU ','Mouse '];
 document.write(device);
 
-device = ['Keyboard ',];
-document.write(device);
 
-device = ['CPU ',];
-document.write(device);
-
-device = ['Mouse '];
-document.write(device);
-
-/////////////////////////////////////
-
-device = device[0];
-device = [ 'Monitor ',];
+device1 = device.shift();
 document.write('<h3>Out</h3>');
-document.write(device);
+document.write(device1);
 
-device = device[1];
-device = [ 'Keyboard ',];
+device2 = device.shift();
 document.write('<h3>Out</h3>');
-document.write(device);
+document.write(device2);
 
-device = device[2];
-device = [ 'CPU ',];
+device3 = device.shift();
 document.write('<h3>Out</h3>');
-document.write(device);
+document.write(device3);
 
-device = device[3];
-device = [ 'Mouse '];
+device4 = device.shift();
 document.write('<h3>Out</h3>');
-document.write(device + '<br>');
+document.write(device4);
+
 
 //question 14
 //from Last calling array
 
 document.write('<h1>New Devices </h1>');
 
-var newdevice = [];
+var device = ['Monitor ','Keyboard ','CPU ','Mouse '];
+document.write(device);
 
-newdevice = ['Monitor ',];
-document.write(newdevice);
-
-newdevice = ['Keyboard ',];
-document.write(newdevice);
-
-newdevice = ['CPU ',];
-document.write(newdevice);
-
-newdevice = ['Mouse '];
-document.write(newdevice);
-
-/////////////////////////////////////
-
-newdevice = newdevice[3];
-newdevice = [ 'Mouse ',];
+device1 = device.pop();
 document.write('<h3>Out</h3>');
-document.write(newdevice);
+document.write(device1);
 
-newdevice = newdevice[2];
-newdevice = [ 'CPU ',];
+device2 = device.pop();
 document.write('<h3>Out</h3>');
-document.write(newdevice);
+document.write(device2);
 
-newdevice = newdevice[1];
-newdevice = [ 'Keyboard ',];
+device3 = device.pop();
 document.write('<h3>Out</h3>');
-document.write(newdevice);
+document.write(device3);
 
-newdevice = newdevice[0];
-newdevice = [ 'Monitor '];
+device4 = device.pop();
 document.write('<h3>Out</h3>');
-document.write(newdevice);
+document.write(device4);
 
 
 //Question 15
@@ -273,14 +216,17 @@ document.write('<h1>Phone Manufacture</h1>');
 // Array to store phone manufacturers
 var phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
 
-// Function to generate dropdown/select menu
-function generateDropdown() {
-    document.write("<select>");
-    for (var i = 0; i < phoneManufacturers.length; i++) {
-        document.write("<option value='" + phoneManufacturers[i] + "'>" + phoneManufacturers[i] + "</option>");
-    }
-    document.write("</select>");
-}
+// Display to generate dropdown/select menu
 
-// Call the function to generate the dropdown/select menu
-generateDropdown();
+    document.write("<select>");
+    
+        document.write('<option>'  + phoneManufacturers[0] + '</option>');
+        document.write('<option>'  + phoneManufacturers[1] + '</option>');
+        document.write('<option>'  + phoneManufacturers[2] + '</option>');
+        document.write('<option>'  + phoneManufacturers[3] + '</option>');
+        document.write('<option>'  + phoneManufacturers[4] + '</option>');
+        document.write('<option>'  + phoneManufacturers[5] + '</option>');
+
+    document.write("</select>");
+
+
