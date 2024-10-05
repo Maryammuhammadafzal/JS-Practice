dead0ecde5b4a8dc958bdc55144c4f4969c8a73b
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-10-05 23:14:04 +0500

    new-assignment

---
+++ Last-Chapter-Object/app.js
@@ -0,0 +1,13 @@
+// Question 1
+
+var itemsArray = [
+    { name: "juice", price: 50, quantity: 3 },
+    { name: "cookie", price: 30, quantity: 9 },
+    { name: "shirt", price: 880, quantity: 1 },
+    { name: "pen", price: 100, quantity: 2 }];
+
+// Calculate total price of each item and all items; 
+
+var totalPrice = (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) 
+
+console.log(totalPrice);
