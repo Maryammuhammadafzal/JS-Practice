e893880d471db65e344110b09b267a88dd042337
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-26 00:23:00 +0500

    add first assignment

--- Last-Chapter-Object/app.js
+++ Last-Chapter-Object/app.js
@@ -8,6 +8,6 @@ var itemsArray = [
 
 // Calculate total price of each item and all items; 
 
-var totalPrice = (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[0].price * itemsArray[0].quantity) 
+ var totalPrice = (itemsArray[0].price * itemsArray[0].quantity) + (itemsArray[1].price * itemsArray[1].quantity) + (itemsArray[2].price * itemsArray[2].quantity) + (itemsArray[3].price * itemsArray[3].quantity) 
 
 console.log(totalPrice);
